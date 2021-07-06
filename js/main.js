f76c36d5475806211c245287e0b200252e67ee96
PARA-dok-s <kristy94@bk.ru>
2021-07-06 21:18:22 +0300

    add pop-up

--- js/main.js
+++ js/main.js
@@ -1,42 +1,68 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-  // effect: 'cube',
+    // effect: 'cube',
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+
+    // effect: 'cube',
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  var menuButton = $(".button-menu");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    console.log(closeModal);
+  }
 });
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-
-  // effect: 'cube',
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-var menuButton = document.querySelector(".button-menu");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+$(openModal).keydown(function (eventObject) {
+  if (eventObject.which == 27) {
+    do_function(closeModal);
+  }
 });
