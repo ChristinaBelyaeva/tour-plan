0af6367f7dc62e34ab2718b7a7f7670130cb6daf
Christina <kristy94@bk.ru>
2021-11-12 17:07:30 +0300

    hide scroll

--- js/main.js
+++ js/main.js
@@ -35,28 +35,31 @@ $(document).ready(function () {
     },
   });
 
-  var menuButton = $(".button-menu");
+  let menuButton = $(".button-menu");
   menuButton.on("click", function () {
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+    $(".body").toggleClass("body--hidden");
   });
 
-  var modalButton = $("[data-toggle=modal]");
-  var closeModalButton = $(".modal__close");
+  let modalButton = $("[data-toggle=modal]");
+  let closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
   function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
+    document.querySelector(".body").style.overflow = "hidden";
   }
   function closeModal(event) {
     event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
+    let modalOverlay = $(".modal__overlay");
+    let modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
+    document.querySelector(".body").style.overflow = "";
     console.log(closeModal);
   }
   // Обработка форм
